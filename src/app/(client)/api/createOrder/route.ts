2cfebd14015ad753820838bee858b6e377d7fa41
RamBh07 <154586316+RamBh07@users.noreply.github.com>
2025-10-21 11:26:52 +0530

    new

--- src/app/(client)/api/createOrder/route.ts
+++ src/app/(client)/api/createOrder/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 import Razorpay from 'razorpay';
 
-
+  const razorpay=  new Razorpay({ key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!, key_secret: process.env.RAZORPAY_SECRET_ID! })
 
 export async function POST(req: Request) {
-  const razorpay=  new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID!, key_secret: process.env.RAZORPAY_SECRET_ID! })
+
   try {
     const body = await req.json();
 
